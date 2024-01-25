e3eff2337ba18abe067c4d5a1ac1452ad9f60a6d
ledong0110 <ducdong0110@gmail.com>
2024-01-25 15:14:23 +0700

    feat: add lab1

---
+++ lab1/adafruit_mqtt.py
@@ -0,0 +1,38 @@
+import sys
+from Adafruit_IO import MQTTClient
+
+class Adafruit_MQTT:
+    AIO_FEED_IDs = ["button1", 	"button2"]
+    AIO_USERNAME = "NPNLab_"
+    AIO_KEY = "aio_"
+
+    def connected(self, client):
+        print("Connected ...")
+        for feed in self.AIO_FEED_IDs:
+            client.subscribe(feed)
+
+    def subscribe(self, lient , userdata , mid , granted_qos):
+        print("Subscribed...")
+    
+    def publish(self, feed_id, content):
+        self.client.publish(feed_id, content)
+
+    def disconnected(self, client):
+        print("Disconnected...")
+        sys.exit (1)
+
+    def message(self, client , feed_id , payload):
+        print("Received: '{}', Feed id: '{}'".format(payload, feed_id))
+
+    def __init__(self, username, key, feed_ids):
+        self.AIO_USERNAME=username
+        self.AIO_KEY=key
+        self.AIO_FEED_IDS=feed_ids
+        self.client = MQTTClient(self.AIO_USERNAME , self.AIO_KEY)
+        self.client.on_connect = self.connected
+        self.client.on_disconnect = self.disconnected
+        self.client.on_message = self.message
+        self.client.on_subscribe = self.subscribe
+        self.client.connect()
+        self.client.loop_background()
+
--- lab1/main.py
+++ lab1/main.py
@@ -1,31 +1,35 @@
-import sys
-from Adafruit_IO import MQTTClient
+from adafruit_mqtt import Adafruit_MQTT
+import time
+import random
 
-AIO_FEED_ID = "button1"
+AIO_FEED_ID = ["button1", "button2"]#, "sensor1", "sensor2", "sensor3", "ai"]
 AIO_USERNAME = "ledong0110"
-AIO_KEY = "aio_LUHO39G4CFQ2mVJbiWAlIVFLN2kj"
+AIO_KEY = "aio_iYyE93rB64dcXAweBfnCdt2ftFK2"
 
-def connected(client):
-    print("Ket noi thanh cong ...")
-    client.subscribe(AIO_FEED_ID)
-
-def subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong ...")
-
-def disconnected(client):
-    print("Ngat ket noi ...")
-    sys.exit (1)
-
-def message(client , feed_id , payload):
-    print("Nhan du lieu: " + payload)
-
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
-client.on_connect = connected
-client.on_disconnect = disconnected
-client.on_message = message
-client.on_subscribe = subscribe
-client.connect()
-client.loop_background()
-
+obj = Adafruit_MQTT(username=AIO_USERNAME, key=AIO_KEY, feed_ids=AIO_FEED_ID)
+counter = 10
+sensor_type = 0
 while True:
-    pass
+    counter -= 1
+    if counter <= 0:
+        counter = 10
+        print("Random data is publishing...")
+        if sensor_type == 0:
+            print("Temperature...")
+            temp = random.randint(10,20)
+            obj.publish("sensor1", temp)
+            sensor_type = 1
+        elif sensor_type == 1:
+            print("Brightness...")
+            brightness = random.randint(100,500)
+            obj.publish("sensor2", brightness)
+            sensor_type = 2
+        elif sensor_type == 2:
+            print("Humidity...")
+            humi = random.randint(50,70)
+            obj.publish("sensor3", humi)
+            sensor_type = 0
+        else:
+            pass
+            
+    time.sleep(1)
