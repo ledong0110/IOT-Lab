46f41615893fba672d3e9d2fc1e897e0b1dbf0bc
ledong0110 <ducdong0110@gmail.com>
2024-04-03 16:22:38 +0700

    feat: add lab 3

--- lab3/main.py
+++ lab3/main.py
@@ -29,26 +29,26 @@ previousResult = aiResult = ""
 while True:
     counter -= 1
     counter_ai -= 1
-    if counter <= 0:
-        counter = 10
-        print("Random data is publishing...")
-        if sensor_type == 0:
-            print("Temperature...")
-            temp = random.randint(10,20)
-            obj.publish("sensor1", temp)
-            sensor_type = 1
-        elif sensor_type == 1:
-            print("Brightness...")
-            brightness = random.randint(100,500)
-            obj.publish("sensor2", brightness)
-            sensor_type = 2
-        elif sensor_type == 2:
-            print("Humidity...")
-            humi = random.randint(50,70)
-            obj.publish("sensor3", humi)
-            sensor_type = 0
-        else:
-            pass
+    # if counter <= 0:
+    #     counter = 10
+    #     print("Random data is publishing...")
+    #     if sensor_type == 0:
+    #         print("Temperature...")
+    #         temp = random.randint(10,20)
+    #         obj.publish("sensor1", temp)
+    #         sensor_type = 1
+    #     elif sensor_type == 1:
+    #         print("Brightness...")
+    #         brightness = random.randint(100,500)
+    #         obj.publish("sensor2", brightness)
+    #         sensor_type = 2
+    #     elif sensor_type == 2:
+    #         print("Humidity...")
+    #         humi = random.randint(50,70)
+    #         obj.publish("sensor3", humi)
+    #         sensor_type = 0
+    #     else:
+    #         pass
     
     if counter_ai <= 0:
         counter_ai = 5
